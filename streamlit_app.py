cb86acd60a665622a6a0388b82193fdcccd37251
clay <claycampbell@gmail.com>
2023-05-24 11:50:50 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
+def main():
+    st.title("My Streamlit App")
+    st.write("Welcome to my app!")
+    name = st.text_input("Enter your name")
+    st.write(f"Hello, {name}!")
+
+if __name__ == "__main__":
+    main()
