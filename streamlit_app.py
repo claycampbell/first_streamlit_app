be5b2452d889f4f514c588e9b4ca0177be23aa4c
clay <claycampbell@gmail.com>
2023-05-24 11:51:56 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
-def main():
-    st.title("My Streamlit App")
-    st.write("Welcome to my app!")
-    name = st.text_input("Enter your name")
-    st.write(f"Hello, {name}!")
-
-if __name__ == "__main__":
-    main()
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
