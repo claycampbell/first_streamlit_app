fa700c76ff9bbee500eefcb9c7aff8cd23c5b2ac
clay <claycampbell@gmail.com>
2023-05-25 11:13:20 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -79,20 +79,17 @@ async def main():
 
     st.divider()
 
-    if st.session_state['ready']:
-        if 'generated' not in st.session_state:
-            st.session_state['generated'] = ["Welcome! You can now ask any questions regarding " + uploaded_file.name]
+if st.session_state['ready']:
+    if 'generated' not in st.session_state:
+        st.session_state['generated'] = ["Welcome! You can now ask any questions regarding " + uploaded_file.name]
 
-        if 'past' not in st.session_state:
-            st.session_state['past'] = ["Hey!"]
+    if 'past' not in st.session_state:
+        st.session_state['past'] = ["Hey!"]
 
-        # Container for chat history
-        response_container = st.container()
+    # Container for chat history
+    response_container = st.container()
 
-        # Container for text box
-        container = st.container()
-
-       with container:
+    # Container for text box
     with st.form(key='my_form', clear_on_submit=True):
         user_input = st.text_input("Query:", placeholder="e.g: Summarize the document in a few sentences", key='input')
         submit_button = st.form_submit_button(label='Send')
@@ -102,11 +99,11 @@ async def main():
         st.session_state['past'].append(user_input)
         st.session_state['generated'].append(output)
 
-if st.session_state['generated']:
-    with response_container:
-        for i in range(len(st.session_state['generated'])):
-            message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="thumbs")
-            message(st.session_state["generated"][i], key=str(i), avatar_style="fun-emoji")
+    if st.session_state['generated']:
+        with response_container:
+            for i in range(len(st.session_state['generated'])):
+                message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="thumbs")
+                message(st.session_state["generated"][i], key=str(i), avatar_style="fun-emoji")
 
 
 if __name__ == "__main__":
