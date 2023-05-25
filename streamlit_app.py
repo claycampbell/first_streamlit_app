da4895e0e0a95d3b2482922d0197da7cdcf0c561
clay <claycampbell@gmail.com>
2023-05-25 11:11:25 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -92,21 +92,21 @@ async def main():
         # Container for text box
         container = st.container()
 
-        with container:
-                        with st.form(key='my_form', clear_on_submit=True):
-                user_input = st.text_input("Query:", placeholder="e.g: Summarize the document in a few sentences", key='input')
-                submit_button = st.form_submit_button(label='Send')
+       with container:
+    with st.form(key='my_form', clear_on_submit=True):
+        user_input = st.text_input("Query:", placeholder="e.g: Summarize the document in a few sentences", key='input')
+        submit_button = st.form_submit_button(label='Send')
 
-            if submit_button and user_input:
-                output = await conversational_chat(user_input)
-                st.session_state['past'].append(user_input)
-                st.session_state['generated'].append(output)
+    if submit_button and user_input:
+        output = await conversational_chat(user_input)
+        st.session_state['past'].append(user_input)
+        st.session_state['generated'].append(output)
 
-        if st.session_state['generated']:
-            with response_container:
-                for i in range(len(st.session_state['generated'])):
-                    message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="thumbs")
-                    message(st.session_state["generated"][i], key=str(i), avatar_style="fun-emoji")
+if st.session_state['generated']:
+    with response_container:
+        for i in range(len(st.session_state['generated'])):
+            message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="thumbs")
+            message(st.session_state["generated"][i], key=str(i), avatar_style="fun-emoji")
 
 
 if __name__ == "__main__":
