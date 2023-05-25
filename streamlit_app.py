0f6b3d331991cdedb513ad460b81aabab2f473e3
clay <claycampbell@gmail.com>
2023-05-25 11:02:55 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,50 +1,121 @@
-import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import ConversationalRetrievalChain
+import pickle
+from pathlib import Path
+from dotenv import load_dotenv
 import os
-import openai
+import streamlit as st
+from streamlit_chat import message
+import io
+import asyncio
 
-# Set page title and icon
-st.set_page_config(page_title="User Story Generation", page_icon=":memo:")
+load_dotenv()
+api_key = os.getenv('OPENAI_API_KEY')  
 
-# Set up OpenAI API credentials
-os.environ['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY']
+# vectors = getDocEmbeds("gpt4.pdf")
+# qa = ChatVectorDBChain.from_llm(ChatOpenAI(model_name="gpt-3.5-turbo"), vectors, return_source_documents=True)
 
-# Function to preprocess the BRD text (if needed)
-def preprocess_brd_text(file_contents):
-    # Implement your preprocessing logic here
-    # This function should clean and format the text as needed
-    # You can remove unnecessary sections, perform text cleaning, etc.
-    processed_text = file_contents
-    return processed_text
+async def main():
 
-# Function to generate user stories
-def generate_user_stories(processed_text):
-    # Generate user stories using OpenAI API
-    response = openai.Completion.create(
-        engine="gpt-3.5-turbo",
-        prompt=processed_text,
-        max_tokens=1000,
-        temperature=0.7,
-        n=5,
-        stop=None,
-        echo=True
-    )
+    async def storeDocEmbeds(file, filename):
+    
+        reader = PdfReader(file)
+        corpus = ''.join([p.extract_text() for p in reader.pages if p.extract_text()])
+        
+        splitter =  RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200,)
+        chunks = splitter.split_text(corpus)
+        
+        embeddings = OpenAIEmbeddings(openai_api_key = api_key)
+        vectors = FAISS.from_texts(chunks, embeddings)
+        
+        with open(filename + ".pkl", "wb") as f:
+            pickle.dump(vectors, f)
 
-    # Extract the generated user stories from the API response
-    user_stories = response.choices[0].text.strip().split('\n')
+        
+    async def getDocEmbeds(file, filename):
+        
+        if not os.path.isfile(filename + ".pkl"):
+            await storeDocEmbeds(file, filename)
+        
+        with open(filename + ".pkl", "rb") as f:
+            global vectores
+            vectors = pickle.load(f)
+            
+        return vectors
+    
 
-    # Return the user stories
-    return user_stories
+    async def conversational_chat(query):
+        result = qa({"question": query, "chat_history": st.session_state['history']})
+        st.session_state['history'].append((query, result["answer"]))
+        # print("Log: ")
+        # print(st.session_state['history'])
+        return result["answer"]
 
-# File upload and user story generation
-st.title("User Story Generation")
-uploaded_file = st.file_uploader("Upload your Business Requirements Document (BRD)", type="txt")
 
-if uploaded_file is not None:
-    file_contents = uploaded_file.read().decode("utf-8")
-    processed_text = preprocess_brd_text(file_contents)
-    user_stories = generate_user_stories(processed_text)
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo")
+    chain = load_qa_chain(llm, chain_type="stuff")
 
-    # Display user stories
-    st.header("Generated User Stories")
-    for i, story in enumerate(user_stories, start=1):
-        st.write(f"User Story {i}: {story}")
+    if 'history' not in st.session_state:
+        st.session_state['history'] = []
+
+
+    #Creating the chatbot interface
+    st.title("PDFChat :")
+
+    if 'ready' not in st.session_state:
+        st.session_state['ready'] = False
+
+    uploaded_file = st.file_uploader("Choose a file", type="pdf")
+
+    if uploaded_file is not None:
+
+        with st.spinner("Processing..."):
+        # Add your code here that needs to be executed
+            uploaded_file.seek(0)
+            file = uploaded_file.read()
+            # pdf = PyPDF2.PdfFileReader()
+            vectors = await getDocEmbeds(io.BytesIO(file), uploaded_file.name)
+            qa = ConversationalRetrievalChain.from_llm(ChatOpenAI(model_name="gpt-3.5-turbo"), retriever=vectors.as_retriever(), return_source_documents=True)
+
+        st.session_state['ready'] = True
+
+    st.divider()
+
+    if st.session_state['ready']:
+
+        if 'generated' not in st.session_state:
+            st.session_state['generated'] = ["Welcome! You can now ask any questions regarding " + uploaded_file.name]
+
+        if 'past' not in st.session_state:
+            st.session_state['past'] = ["Hey!"]
+
+        # container for chat history
+        response_container = st.container()
+
+        # container for text box
+        container = st.container()
+
+        with container:
+            with st.form(key='my_form', clear_on_submit=True):
+                user_input = st.text_input("Query:", placeholder="e.g: Summarize the paper in a few sentences", key='input')
+                submit_button = st.form_submit_button(label='Send')
+
+            if submit_button and user_input:
+                output = await conversational_chat(user_input)
+                st.session_state['past'].append(user_input)
+                st.session_state['generated'].append(output)
+
+        if st.session_state['generated']:
+            with response_container:
+                for i in range(len(st.session_state['generated'])):
+                    message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="thumbs")
+                    message(st.session_state["generated"][i], key=str(i), avatar_style="fun-emoji")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
