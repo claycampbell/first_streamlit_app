d92180d5c8095ffab155103e410ba3ea303340fc
clay <claycampbell@gmail.com>
2023-05-25 10:38:06 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,49 @@
 import streamlit as st
 import os
-from file_processor import process_file
+import openai
 
 # Set page title and icon
 st.set_page_config(page_title="User Story Generation", page_icon=":memo:")
 
-# File upload
+# Set up OpenAI API credentials
+openai_api_key = st.secrets["OPENAI_API_KEY"]
+openai.api_key = openai_api_key
+
+# Preprocess function (if needed)
+def preprocess_brd_text(file_contents):
+    # Implement your preprocessing logic here
+    # This function should clean and format the text as needed
+    # You can remove unnecessary sections, perform text cleaning, etc.
+    processed_text = file_contents
+    return processed_text
+
+# User story generation function
+def generate_user_stories(processed_text):
+    # Generate user stories using OpenAI API
+    response = openai.Completion.create(
+        engine="text-davinci-0035-turbo",  # Use GPT-3.5 Turbo engine
+        prompt=processed_text,
+        max_tokens=1000,  # Adjust as needed
+        temperature=0.7,  # Adjust as needed
+        n=5,  # Number of user stories to generate
+        stop=None,  # Stop condition to end the generated text, if necessary
+        echo=True  # To include the prompt in the generated text
+    )
+
+    # Extract the generated user stories from the API response
+    user_stories = response.choices[0].text.strip().split('\n')
+
+    # Return the user stories
+    return user_stories
+
+# File upload and user story generation
 st.title("User Story Generation")
 uploaded_file = st.file_uploader("Upload your Business Requirements Document (BRD)", type="txt")
 
-# Process file and generate user stories
 if uploaded_file is not None:
     file_contents = uploaded_file.read().decode("utf-8")
-    user_stories = process_file(file_contents)
+    processed_text = preprocess_brd_text(file_contents)
+    user_stories = generate_user_stories(processed_text)
 
     # Display user stories
     st.header("Generated User Stories")
