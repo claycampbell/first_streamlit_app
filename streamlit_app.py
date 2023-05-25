2759e04383aee7189dbbb08fc1d32415426849b2
clay <claycampbell@gmail.com>
2023-05-24 19:43:51 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,44 +1,22 @@
+pip install streamlit
+
 import streamlit as st
-import pandas as pd
+import os
+from file_processor import process_file
 
-st.title('My Parents New Healthy Diner')
-st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-st.text('Omega 3 & Blueberry Oatmeal')
-st.text('Kale, Spinach & Rocket Smoothie')
-st.text('Hard-Boiled Free-Range Egg')
+# Set page title and icon
+st.set_page_config(page_title="User Story Generation", page_icon=":memo:")
 
-my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')  # Fix the line to set the index
-fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
-st.dataframe(fruits_to_show)
+# File upload
+st.title("User Story Generation")
+uploaded_file = st.file_uploader("Upload your Business Requirements Document (BRD)", type="txt")
 
-st.header("Fruityvice Fruit Advice!")
-fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
-st.write('The user entered ', fruit_choice)
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+# Process file and generate user stories
+if uploaded_file is not None:
+    file_contents = uploaded_file.read().decode("utf-8")
+    user_stories = process_file(file_contents)
 
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-st.dataframe(fruityvice_normalized)
-
-import snowflake.connector
-
-my_cnx = snowflake.connector.connect(**st.secrets ["snowflake"])
-# Get the fruit load list from Snowflake
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * FROM fruit_load_list")
-my_data_rows = my_cur.fetchall()
-
-# Display the fruit load list
-st.header("The fruit load list contains:")
-st.dataframe(my_data_rows)
-
-# Add a fruit to the list using user input
-new_fruit = st.text_input("Add a fruit to the list:")
-if new_fruit:
-    my_cur.execute("INSERT INTO fruit_load_list (fruit_name) VALUES (%s)", (new_fruit,))
-    my_cnx.commit()
-    st.success("Fruit added successfully!")
+    # Display user stories
+    st.header("Generated User Stories")
+    for i, story in enumerate(user_stories, start=1):
+        st.write(f"User Story {i}: {story}")
