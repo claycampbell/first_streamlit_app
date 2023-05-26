b66aaa21562ba855034250f920a4abd6b00f2573
clay <claycampbell@gmail.com>
2023-05-25 19:33:55 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,25 @@
+import asyncio
+import io
+import os
+import pickle
+from pathlib import Path
+
+import streamlit as st
+from dotenv import load_dotenv
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import ConversationalRetrievalChain
-import pickle
-from pathlib import Path
-from dotenv import load_dotenv
-import os
-import streamlit as st
 from streamlit_chat import message
-import io
-import asyncio
 
 load_dotenv()
-api_key = os.getenv('OPENAI_API_KEY')  
+api_key = os.getenv('OPENAI_API_KEY')
+
 
 async def main():
-    async def storeDocEmbeds(file, filename):
+    async def store_doc_embeddings(file, filename):
         corpus = file.read().decode("utf-8")
 
         splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
@@ -29,9 +31,9 @@ async def main():
         with open(filename + ".pkl", "wb") as f:
             pickle.dump(vectors, f)
 
-    async def getDocEmbeds(file, filename):
+    async def get_doc_embeddings(file, filename):
         if not os.path.isfile(filename + ".pkl"):
-            await storeDocEmbeds(file, filename)
+            await store_doc_embeddings(file, filename)
 
         with open(filename + ".pkl", "rb") as f:
             vectors = pickle.load(f)
@@ -63,23 +65,32 @@ async def main():
             file = uploaded_file.read()
 
             if uploaded_file.type == "pdf":
-                vectors = await getDocEmbeds(io.BytesIO(file), uploaded_file.name)
+                vectors = await get_doc_embeddings(io.BytesIO(file), uploaded_file.name)
+                qa = ConversationalRetrievalChain.from_llm(
+                    ChatOpenAI(model_name="gpt-3.5-turbo"),
+                    retriever=vectors.as_retriever(),
+                    return_source_documents=True
+                )
             elif uploaded_file.type == "docx":
-                vectors = await getDocEmbeds(io.BytesIO(file), uploaded_file.name)
+                vectors = await get_doc_embeddings(io.BytesIO(file), uploaded_file.name)
+                qa = ConversationalRetrievalChain.from_llm(
+                    ChatOpenAI(model_name="gpt-3.5-turbo"),
+                    retriever=vectors.as_retriever(),
+                    return_source_documents=True
+                )
             elif uploaded_file.type == "txt":
-                vectors = await getDocEmbeds(io.BytesIO(file), uploaded_file.name)
+                vectors = await get_doc_embeddings(io.BytesIO(file), uploaded_file.name)
+                qa = ConversationalRetrievalChain.from_llm(
+                    ChatOpenAI(model_name="gpt-                    3.5-turbo"),
+                    retriever=vectors.as_retriever(),
+                    return_source_documents=True
+                )
 
-            qa = ConversationalRetrievalChain.from_llm(
-                ChatOpenAI(model_name="gpt-3.5-turbo"),
-                retriever=vectors.as_retriever(),
-                return_source_documents=True
-            )
-
-        st.session_state['ready'] = True
+            st.session_state['ready'] = True
 
     st.divider()
 
-    if st.session_state.get('ready', False):
+    if st.session_state['ready']:
         if 'generated' not in st.session_state:
             st.session_state['generated'] = ["Welcome! You can now ask any questions regarding " + uploaded_file.name]
 
@@ -94,19 +105,21 @@ async def main():
             user_input = st.text_input("Query:", placeholder="e.g: Summarize the document in a few sentences", key='input')
             submit_button = st.form_submit_button(label='Send')
 
-        if submit_button and user_input:
-            output = await conversational_chat(user_input)
-            st.session_state['past'].append(user_input)
-            st.session_state['generated'].append(output)
+            if submit_button and user_input:
+                loop = asyncio.new_event_loop()
+                output = await conversational_chat(user_input)
+                loop.close()
 
-    if st.session_state.get('generated', []):
-        with response_container:
-            for i in range(len(st.session_state['generated'])):
-                message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="thumbs")
-                message(st.session_state["generated"][i], key=str(i), avatar_style="fun-emoji")
+                st.session_state['past'].append(user_input)
+                st.session_state['generated'].append(output)
+
+        if st.session_state['generated']:
+            with response_container:
+                for i in range(len(st.session_state['generated'])):
+                    message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="thumbs")
+                    message(st.session_state["generated"][i], key=str(i), avatar_style="fun-emoji")
 
 
 if __name__ == "__main__":
     asyncio.run(main())
 
-                     
