808881a1bafb72e4f244cc01274e1df3c5d7ba60
clay <claycampbell@gmail.com>
2023-05-29 13:32:14 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,123 +1,31 @@
-from PyPDF2 import PdfReader
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores import FAISS
-from langchain.chains.question_answering import load_qa_chain
-from langchain.chat_models import ChatOpenAI
-from langchain.chains import ConversationalRetrievalChain
-import pickle
-from pathlib import Path
-from dotenv import load_dotenv
-import os
 import streamlit as st
-from streamlit_chat import message
-import io
-import asyncio
-
-load_dotenv()
-api_key = os.getenv('OPENAI_API_KEY')
+from flow_coordinator import run
 
 
-async def storeDocEmbeds(file, filename):
-    reader = PdfReader(file)
-    corpus = ''.join([p.extract_text() for p in reader.pages if p.extract_text()])
-
-    splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200, )
-    chunks = splitter.split_text(corpus)
-
-    embeddings = OpenAIEmbeddings(openai_api_key=api_key)
-    vectors = FAISS.from_texts(chunks, embeddings)
-
-    with open(filename + ".pkl", "wb") as f:
-        pickle.dump(vectors, f)
-
-
-async def getDocEmbeds(file, filename):
-    if not os.path.isfile(filename + ".pkl"):
-        await storeDocEmbeds(file, filename)
-
-    with open(filename + ".pkl", "rb") as f:
-        global vectors
-        vectors = pickle.load(f)
-
-    return vectors
-
-
-async def conversational_chat(query):
-    result = qa({"question": query, "chat_history": st.session_state['history']})
-    st.session_state['history'].append((query, result["answer"]))
-    return result["answer"]
-
-
-def send_quick_message():
-    query = "Quick message"  # Set the desired quick message
-    asyncio.create_task(conversational_chat(query))
 
 
 def main():
-    llm = ChatOpenAI(model_name="gpt-3.5-turbo")
-    chain = load_qa_chain(llm, chain_type="stuff")
+    """
+    Main function to run the Streamlit interface and execute the run function from file_processor.
+    This file follows the dependency inversion principle, and separates UI from backend functionalities:
+    Streamlit framework is used to render the GUI and manage file uploads.
+    To use other approaches or frameworks: 
+    1) Make sure to set up a GUI that allows users to upload files & enter text input for their questions.
+    2) Import the run method from the flow_coordinator.py
+    3) Get the response by running the run(files, user_question) method with the appropriate arguments. 
 
-    if 'history' not in st.session_state:
-        st.session_state['history'] = []
+    
+    """
 
-    # Creating the chatbot interface
-    st.title("PDFChat :")
+    st.set_page_config(page_title="Talk With Your Files")  # can add parameters to style the page
+    st.header("Talk With Your Files")
 
-    if 'ready' not in st.session_state:
-        st.session_state['ready'] = False
+    files = st.file_uploader("Upload files", type=["pdf", "docx", "txt","csv"], accept_multiple_files=True)
+    user_question = st.text_input("Please ask me about your uploaded files and I shall help you: ")
 
-    uploaded_file = st.file_uploader("Choose a file", type="pdf")
+    if user_question and files:
+        response = run(files, user_question)
+        st.write(response)
 
-    if uploaded_file is not None:
-
-        with st.spinner("Processing..."):
-            uploaded_file.seek(0)
-            file = uploaded_file.read()
-            vectors = await getDocEmbeds(io.BytesIO(file), uploaded_file.name)
-            qa = ConversationalRetrievalChain.from_llm(ChatOpenAI(model_name="gpt-3.5-turbo"),
-                                                      retriever=vectors.as_retriever(),
-                                                      return_source_documents=True)
-
-        st.session_state['ready'] = True
-
-    st.divider()
-
-    if st.session_state['ready']:
-
-        if 'generated' not in st.session_state:
-            st.session_state['generated'] = ["Welcome! You can now ask any questions regarding " + uploaded_file.name]
-
-        if 'past' not in st.session_state:
-            st.session_state['past'] = ["Hey!"]
-
-        # container for chat history
-        response_container = st.container()
-
-        # container for text box
-        container = st.container()
-
-        with container:
-            user_input = st.text_input("Query:", placeholder="e.g: Summarize the paper in a few sentences", key='input')
-            submit_button = st.button('Send')
-
-        if submit_button and user_input:
-            output = await conversational_chat(user_input)
-            st.session_state['past'].append(user_input)
-            st.session_state['generated'].append(output)
-
-        quick_message_button = st.button("Quick Message")
-
-        if quick_message_button:
-            send_quick_message()
-
-        if st.session_state['generated']:
-            with response_container:
-                for i in range(len(st.session_state['generated'])):
-                    message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="thumbs")
-                    message(st.session_state["generated"][i], key=str(i), avatar_style="fun-emoji")
-
-
-if __name__ == "__main__":
-    st.experimental_rerun()  # Re-run the Streamlit app to avoid asyncio event loop issues
-    st.experimental_asyncio.run(main())
+if __name__ == '__main__':
+    main()
